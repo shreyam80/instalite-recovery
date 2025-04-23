af795f6a3788cf9b4b8c3d4b031bae6d802cf3f2
jimena-olr <jimenao@seas.upenn.edu>
2025-04-23 00:21:46 -0400

    fixed merge conflict problem

--- instalite-backend/routes/routes.js
+++ instalite-backend/routes/routes.js
@@ -15,9 +15,6 @@ import {
   getUserChats
 } from '../../server/chat/chat.js';
 
-// ---------- AUTH ----------
->>>>>>> 3215ec61b955d9375d655f67cbfec46e9b573df7
-
 export async function handleLogin(req, res) {
   const result = await authenticateUser(req.body);
   if (result.error) return res.status(401).json({ error: result.error });
